01ef55c517e3e03a57af06bff5bc9874ddff929f
Denzell Grant <denzgrant@gmail.com>
2020-05-12 23:14:25 -0700

    set up api routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,27 @@
 const express = require("express"); 
 const path = require("path");
+const db = require("../db/db.json"); //not working :/ 
+const fs = require('fs'); 
 
 const app = express(); 
 app.use(express.static(path.join(__dirname, '/public')));
 const PORT = process.env.PORT || 8888 ; 
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public")); 
 
 
+let data = fs.readFile('db.json')
+let newdata = JSON.parse(data); 
+console.log(newdata); 
+
 app.get("/", function (req, res) {
+    console.log(`/index called`);
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/*", function (req, res) {
     console.log(`/index called`);
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
@@ -19,7 +29,14 @@ app.get("/", function (req, res) {
 app.get("/notes", function (req, res) {
     console.log(`/notes called`);
     res.sendFile(path.join(__dirname, "/public/notes.html")); 
-}); 
+});  
+
+// app.get("/api/db", (req, res) => {
+    // res.jsondb;
+// ;
+// app.post("/api/db", (req, res) => {
+    // res.jsondb;
+// ;
 
 
 app.listen(PORT, () => {

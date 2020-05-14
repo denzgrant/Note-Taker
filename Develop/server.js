721941fba4e65a3b8f517c3c67bb4cc35984b1d9
Denzell Grant <denzgrant@gmail.com>
2020-05-13 23:35:05 -0700

    updated API routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require("express"); 
 const path = require("path");
-const db = require("../db/db.json"); //not working :/ 
+const db = require("./db/db.json"); 
 const fs = require('fs'); 
 
 const app = express(); 
@@ -12,16 +12,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public")); 
 
 
-let data = fs.readFile('db.json')
-let newdata = JSON.parse(data); 
-console.log(newdata); 
+// let data = fs.readFile('db.json')
+// let newdata = JSON.parse(data); 
+// console.log(newdata); 
 
-app.get("/", function (req, res) {
-    console.log(`/index called`);
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
 
-app.get("/*", function (req, res) {
+app.get("/8", function (req, res) {
     console.log(`/index called`);
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
@@ -31,12 +27,29 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html")); 
 });  
 
-// app.get("/api/db", (req, res) => {
-    // res.jsondb;
-// ;
-// app.post("/api/db", (req, res) => {
-    // res.jsondb;
-// ;
+app.get("/api/notes", (req, res) => {
+  res.json(db); 
+}); 
+
+// app.post("/api/notes", (req, res) => {
+//     res.readFile(__dirname, "/db/db.json");
+// }); 
+app.post("/api/notes", (req, res) => {
+    console.log(`POST /api/notes called`);
+    const newnote = req.body;
+    newNote.id = (req.body.name.split(" "))[0].toLowerCase();
+
+    console.log(newNote);
+
+    db.push(newnote);
+
+    res.json(newnote);
+});
+
+// app.delete("/api/notes/:id", (req, res) => {
+//     res.params.id(__dirname, "/db/db.json");
+// }); 
+
 
 
 app.listen(PORT, () => {
